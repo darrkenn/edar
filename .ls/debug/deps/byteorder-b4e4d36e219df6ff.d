3e634b9295fa90349abd7ca3f315e6e64e4d865b
darrkenn <darrkenn08@gmail.com>
2025-08-23 17:14:40 +0100

    Remove not required directory

--- .ls/debug/deps/byteorder-b4e4d36e219df6ff.d
+++
@@ -1,8 +0,0 @@
-/home/darragh/Development/edar/.ls/debug/deps/byteorder-b4e4d36e219df6ff.d: /home/darragh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/byteorder-1.5.0/src/lib.rs /home/darragh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/byteorder-1.5.0/src/io.rs
-
-/home/darragh/Development/edar/.ls/debug/deps/libbyteorder-b4e4d36e219df6ff.rlib: /home/darragh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/byteorder-1.5.0/src/lib.rs /home/darragh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/byteorder-1.5.0/src/io.rs
-
-/home/darragh/Development/edar/.ls/debug/deps/libbyteorder-b4e4d36e219df6ff.rmeta: /home/darragh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/byteorder-1.5.0/src/lib.rs /home/darragh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/byteorder-1.5.0/src/io.rs
-
-/home/darragh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/byteorder-1.5.0/src/lib.rs:
-/home/darragh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/byteorder-1.5.0/src/io.rs:
--- .ls/debug/deps/data_encoding-c456abf4e91da2bc.d
+++
@@ -1,7 +0,0 @@
-/home/darragh/Development/edar/.ls/debug/deps/data_encoding-c456abf4e91da2bc.d: /home/darragh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/data-encoding-2.9.0/src/lib.rs
-
-/home/darragh/Development/edar/.ls/debug/deps/libdata_encoding-c456abf4e91da2bc.rlib: /home/darragh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/data-encoding-2.9.0/src/lib.rs
-
-/home/darragh/Development/edar/.ls/debug/deps/libdata_encoding-c456abf4e91da2bc.rmeta: /home/darragh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/data-encoding-2.9.0/src/lib.rs
-
-/home/darragh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/data-encoding-2.9.0/src/lib.rs:
